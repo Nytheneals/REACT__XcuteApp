e78c3a77a4dd021bcc8df9383a5f97456cd2afeb
Nytheneals <nythes@gmail.com>
2018-02-18 12:46:17 -0500

    Created First Commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
 
 import App from './components/app';
-import reducers from './reducers';
 
-ReactDOM.render(
-  <Provider store={createStore(reducers)}>
-    <App />
-  </Provider>,
-  document.querySelector('react_content'),
-);
+ReactDOM.render(<App />, document.querySelector('.container'));
