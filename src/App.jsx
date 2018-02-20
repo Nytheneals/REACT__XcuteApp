9f3f695f773a06751440fb254615eab4534b1e96
Nytheneals <nythes@gmail.com>
2018-02-20 14:53:48 -0500

    Added Delete and Update functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import React, { Component } from 'react';
-import './App.css';
-import NoteCard from './noteCard';
-import firebase from 'firebase';
+import React, { Component } from "react";
+import "./App.css";
+import NoteCard from "./noteCard";
+import firebase from "firebase";
 
 // CONNECTING TO FIREBASE REALTIME DATABASE
 const config = {
-  apiKey: 'AIzaSyBQpt7EfRfLwZlC_PiRgHUBoetUUYZF2oM',
-  authDomain: 'xcute-2ac2f.firebaseapp.com',
-  databaseURL: 'https://xcute-2ac2f.firebaseio.com',
-  projectId: 'xcute-2ac2f',
-  storageBucket: 'xcute-2ac2f.appspot.com',
-  messagingSenderId: '666744767443',
+  apiKey: "AIzaSyBQpt7EfRfLwZlC_PiRgHUBoetUUYZF2oM",
+  authDomain: "xcute-2ac2f.firebaseapp.com",
+  databaseURL: "https://xcute-2ac2f.firebaseio.com",
+  projectId: "xcute-2ac2f",
+  storageBucket: "xcute-2ac2f.appspot.com",
+  messagingSenderId: "666744767443"
 };
 firebase.initializeApp(config);
 
@@ -19,11 +19,13 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      notes: [],
+      notes: []
     };
 
     this.addNote = this.addNote.bind(this);
     this.showSidebar = this.showSidebar.bind(this);
+    this.removeNote = this.removeNote.bind(this);
+    this.updateNote = this.updateNote.bind(this);
   }
 
   // RENDERS AFTER COMPONENT HAS RENDERED
@@ -31,15 +33,20 @@ class App extends Component {
     firebase
       .database()
       .ref()
-      .on('value', (res) => {
+      .on("value", res => {
         const userData = res.val(); // COPY OF THE OBJECT
-        console.log(userData);
         const dataArray = [];
         // ADDING THE KEY TO THE OBJECT
-        for (const key in userData) {
-          userData[key].key = key;
-          dataArray.push(userData[key]);
+        for (const objKey in userData) {
+          userData[objKey].key = objKey; // THIS TAKES THE KEY AND SETS IT AS A KEY
+          // userData[objKey] IS AN OBJECT..
+          dataArray.push(userData[objKey]); // WE ARE PUSHING KEYS INTO THE ARRAY
         }
+        this.setState({
+          notes: dataArray
+        });
+        console.log(userData);
+        console.log(dataArray);
       });
   }
 
@@ -48,7 +55,7 @@ class App extends Component {
   // ADDED A TOGGLE CLASS
   showSidebar(e) {
     e.preventDefault();
-    this.sidebar.classList.toggle('show');
+    this.sidebar.classList.toggle("show");
   }
 
   //* *************** ADD NEW ITEM ****************//
@@ -56,25 +63,31 @@ class App extends Component {
     e.preventDefault();
     const note = {
       title: this.noteTitle.value,
-      text: this.noteText.value,
+      text: this.noteText.value
     };
 
     // MAKING A REFERENCE TO THE FIREBASE DATABASE
     const dbRef = firebase.database().ref();
-    // PUSHING STATE TO THE DB
+    // PUSHING NOTE TO THE DB
     dbRef.push(note);
 
     // REFRESH INPUT FIELDS
-    this.noteTitle.value = '';
-    this.noteText.value = '';
+    this.noteTitle.value = "";
+    this.noteText.value = "";
     this.showSidebar(e);
-    console.table(this.state.notes);
   }
 
   //* ***************** UPDATE ITEM ******************//
-
+  updateNote(noteId) {
+    const dbRef = firebase.database().ref(noteId);
+    dbRef.update();
+  }
   //* ***************** DELETE ITEM ******************//
-
+  removeNote(noteId) {
+    const dbRef = firebase.database().ref(noteId);
+    dbRef.remove();
+  }
+  //***************** COMPONENT RENDER ****************//
   render() {
     return (
       <div>
@@ -89,7 +102,16 @@ class App extends Component {
         </header>
 
         <section className="notes">
-          {this.state.notes.map((note, index) => <NoteCard note={note} key={index} />).reverse()}
+          {this.state.notes
+            .map((note, index) => (
+              <NoteCard
+                note={note}
+                key={index}
+                removeNote={this.removeNote}
+                updateNote={this.updateNote}
+              />
+            ))
+            .reverse()}
         </section>
 
         <aside className="sidebar" ref={ref => (this.sidebar = ref)}>
@@ -99,9 +121,17 @@ class App extends Component {
               <i className="fa fa-times" onClick={this.showSidebar} />
             </div>
             <label htmlFor="note-title">Title:</label>
-            <input type="text" name="note-title" ref={ref => (this.noteTitle = ref)} />
+            <input
+              type="text"
+              name="note-title"
+              ref={ref => (this.noteTitle = ref)}
+            />
             <label htmlFor="note-text">Note:</label>
-            <textarea type="text" name="note-text" ref={ref => (this.noteText = ref)} />
+            <textarea
+              type="text"
+              name="note-text"
+              ref={ref => (this.noteText = ref)}
+            />
             <input type="submit" value="Add New Note" />
           </form>
         </aside>
--- src/noteCard.jsx
+++ src/noteCard.jsx
@@ -1,14 +1,56 @@
-import React from "react";
+import React, { Component } from "react";
 
-// STATELESS COMPONENT
+class NoteCard extends Component {
+  constructor() {
+    super();
+    this.state = {
+      editing: false,
+      note: {}
+    };
+  }
 
-export default function(props) {
-  return (
-    <div className="noteCard">
-      <i className="fa fa-edit" />
-      <i className="fa fa-times" />
-      <h4>{props.note.title}</h4>
-      <p>{props.note.note}</p>
-    </div>
-  );
+  componentDidMount() {
+    this.setState({
+      note: this.props.note
+    });
+  }
+
+  render() {
+    let editingTemp = (
+      <span>
+        <h4>{this.props.note.title}</h4>
+        <p>{this.props.note.text}</p>
+      </span>
+    );
+    if (this.state.editing) {
+      editingTemp = (
+        <form>
+          <div>
+            <input type="text" defaultValue={this.props.note.title} />
+            <input type="text" defaultValue={this.props.note.text} />
+            <input type="submit" value="Done editing" onSubmit={this.update} />
+          </div>
+        </form>
+      );
+    }
+
+    return (
+      <div className="noteCard">
+        <i
+          className="fa fa-edit"
+          onClick={() => {
+            this.setState({ editing: true });
+          }}
+        />
+        <i
+          className="fa fa-times"
+          onClick={() => {
+            this.props.removeNote(this.props.note.key);
+          }}
+        />
+      </div>
+    );
+  }
 }
+
+export default NoteCard;
