577ac542c31c77c5c10557015f63b03e7b5c6d5e
Nytheneals <nythes@gmail.com>
2018-02-20 10:07:41 -0500

    Database Config

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,82 @@
-import React, { Component } from "react";
-import "./App.css";
-import NoteCard from "./noteCard";
-import base from "./base";
+import React, { Component } from 'react';
+import './App.css';
+import NoteCard from './noteCard';
+import firebase from 'firebase';
 
-// STATELESS COMPONENT
+// CONNECTING TO FIREBASE REALTIME DATABASE
+const config = {
+  apiKey: 'AIzaSyBQpt7EfRfLwZlC_PiRgHUBoetUUYZF2oM',
+  authDomain: 'xcute-2ac2f.firebaseapp.com',
+  databaseURL: 'https://xcute-2ac2f.firebaseio.com',
+  projectId: 'xcute-2ac2f',
+  storageBucket: 'xcute-2ac2f.appspot.com',
+  messagingSenderId: '666744767443',
+};
+firebase.initializeApp(config);
 
+// START OF COMPONENT
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      notes: []
+      notes: [],
     };
 
     this.addNote = this.addNote.bind(this);
     this.showSidebar = this.showSidebar.bind(this);
   }
 
-  // SHOW SIDE BAR
-  // CREATED A REFERENCE TO AN ELEMENT, USING A FUNCTION
+  // RENDERS AFTER COMPONENT HAS RENDERED
+  componentDidMount() {
+    firebase
+      .database()
+      .ref()
+      .on('value', (res) => {
+        const userData = res.val(); // COPY OF THE OBJECT
+        console.log(userData);
+        const dataArray = [];
+        // ADDING THE KEY TO THE OBJECT
+        for (const key in userData) {
+          userData[key].key = key;
+          dataArray.push(userData[key]);
+        }
+      });
+  }
 
+  //* *************** SHOW SIDE BAR ****************//
+  // CREATED A REFERENCE TO AN ELEMENT, USING A FUNCTION
   // ADDED A TOGGLE CLASS
   showSidebar(e) {
     e.preventDefault();
-    // console.log(this.sidebar); able to get this because i created a reference c
-
-    this.sidebar.classList.toggle("show");
+    this.sidebar.classList.toggle('show');
   }
 
-  // ADD NEW ITEM
+  //* *************** ADD NEW ITEM ****************//
   addNote(e) {
     e.preventDefault();
-    // MAKING A COPY OF THE STATE
-    const notes = this.state.notes;
-    // PUSH OBJECTS TO STATE ARRAY
-    notes.push({
-      title: this.title.value,
-      note: this.note.value
-    });
-    // UPDATING STATE
-    this.setState({
-      notes: notes
-    });
+    const note = {
+      title: this.noteTitle.value,
+      text: this.noteText.value,
+    };
 
-    console.table(this.state.notes);
-    this.title.value = "";
-    this.note.value = "";
+    // MAKING A REFERENCE TO THE FIREBASE DATABASE
+    const dbRef = firebase.database().ref();
+    // PUSHING STATE TO THE DB
+    dbRef.push(note);
+
+    // REFRESH INPUT FIELDS
+    this.noteTitle.value = '';
+    this.noteText.value = '';
     this.showSidebar(e);
+    console.table(this.state.notes);
   }
 
-  // UPDATE ITEM
+  //* ***************** UPDATE ITEM ******************//
 
-  // DELETE ITEM
+  //* ***************** DELETE ITEM ******************//
 
   render() {
     return (
-      // MAIN HEADER THATS HAS THE LOGO
-
       <div>
         <header className="mainHeader">
           <h1>Xcute`</h1>
@@ -66,12 +87,9 @@ class App extends Component {
             <a href="">Login</a>
           </nav>
         </header>
+
         <section className="notes">
-          {this.state.notes
-            .map((note, index) => {
-              return <NoteCard note={note} key={index} />;
-            })
-            .reverse()}
+          {this.state.notes.map((note, index) => <NoteCard note={note} key={index} />).reverse()}
         </section>
 
         <aside className="sidebar" ref={ref => (this.sidebar = ref)}>
@@ -81,17 +99,9 @@ class App extends Component {
               <i className="fa fa-times" onClick={this.showSidebar} />
             </div>
             <label htmlFor="note-title">Title:</label>
-            <input
-              type="text"
-              name="note-title"
-              ref={ref => (this.title = ref)}
-            />
+            <input type="text" name="note-title" ref={ref => (this.noteTitle = ref)} />
             <label htmlFor="note-text">Note:</label>
-            <textarea
-              type="text"
-              name="note-text"
-              ref={ref => (this.note = ref)}
-            />
+            <textarea type="text" name="note-text" ref={ref => (this.noteText = ref)} />
             <input type="submit" value="Add New Note" />
           </form>
         </aside>
--- src/base.js
+++ src/base.js
@@ -1,7 +1,10 @@
-import { Rebase } from "re-base";
+import Rebase from 're-base';
+
+// CONNECTING TO THE DATABASE
 const base = Rebase.createClass({
-  apiKey: "AIzaSyBQpt7EfRfLwZlC_PiRgHUBoetUUYZF2oM",
-  authDomain: "xcute-2ac2f.firebaseapp.com",
-  databaseURL: "https://xcute-2ac2f.firebaseio.com"
+  apiKey: 'AIzaSyBQpt7EfRfLwZlC_PiRgHUBoetUUYZF2oM',
+  authDomain: 'xcute-2ac2f.firebaseapp.com',
+  databaseURL: 'https://xcute-2ac2f.firebaseio.com',
 });
+
 export default base;
