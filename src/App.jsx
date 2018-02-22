126db60a7f7516a2cc4c7c277e36d6e4ea49a37d
Nytheneals <nythes@gmail.com>
2018-02-21 21:57:31 -0500

    Completed General functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,266 +1,238 @@
-import React, { Component } from "react";
-import "./App.css";
-import NoteCard from "./noteCard";
-import firebase from "firebase";
+import React, { Component } from 'react';
+import './App.css';
+import NoteCard from './noteCard';
+import firebase from 'firebase';
 
 // CONNECTING TO FIREBASE REALTIME DATABASE
 const config = {
-  apiKey: "AIzaSyBQpt7EfRfLwZlC_PiRgHUBoetUUYZF2oM",
-  authDomain: "xcute-2ac2f.firebaseapp.com",
-  databaseURL: "https://xcute-2ac2f.firebaseio.com",
-  projectId: "xcute-2ac2f",
-  storageBucket: "xcute-2ac2f.appspot.com",
-  messagingSenderId: "666744767443"
+  apiKey: 'AIzaSyBQpt7EfRfLwZlC_PiRgHUBoetUUYZF2oM',
+  authDomain: 'xcute-2ac2f.firebaseapp.com',
+  databaseURL: 'https://xcute-2ac2f.firebaseio.com',
+  projectId: 'xcute-2ac2f',
+  storageBucket: 'xcute-2ac2f.appspot.com',
+  messagingSenderId: '666744767443',
 };
 firebase.initializeApp(config);
 
-// START OF COMPONENT
+
 class App extends Component {
+
   constructor() {
-    super();
-    this.state = {
-      notes: []
-    };
-
-    this.addNote = this.addNote.bind(this);
-    this.showSidebar = this.showSidebar.bind(this);
-    this.removeNote = this.removeNote.bind(this);
-    this.updateNote = this.updateNote.bind(this);
-    this.createUser = this.createUser.bind(this);
-    this.showCreate = this.showCreate.bind(this);
-    this.showLogin = this.showLogin.bind(this);
-    this.loginUser = this.loginUser.bind(this);
+      super();
+      this.state = {
+          notes: [],
+          loggedin: false
+      }
+      this.showSideBar = this.showSideBar.bind(this);
+      this.addNote = this.addNote.bind(this);
+      this.showCreate = this.showCreate.bind(this);
+      this.createUser = this.createUser.bind(this);
+      this.showLogin = this.showLogin.bind(this);
+      this.loginUser = this.loginUser.bind(this);
   }
 
-  // RENDERS AFTER COMPONENT HAS RENDERED
   componentDidMount() {
-    firebase
-      .database()
-      .ref()
-      .on("value", res => {
-        const userData = res.val(); // COPY OF THE OBJECT
-        const dataArray = [];
-        // ADDING THE KEY TO THE OBJECT
-        for (const objKey in userData) {
-          userData[objKey].key = objKey; // THIS TAKES THE KEY AND SETS IT AS A KEY
-          // userData[objKey] IS AN OBJECT..
-          dataArray.push(userData[objKey]); // WE ARE PUSHING KEYS INTO THE ARRAY
-        }
-        this.setState({
-          notes: dataArray
-        });
-      });
+      firebase.auth().onAuthStateChanged(user => {
+          if (user) {
+              console.log(user);
+              firebase.database().ref(`users/${user.uid}/notes`).on('value', res => {
+                  const userData = res.val();
+                  const dataArray = [];
+                  for (let objKey in userData) {
+                      userData[objKey].key = objKey;
+                      dataArray.push(userData[objKey]);
+                  }
+                  this.setState({
+                      notes: dataArray,
+                      loggedin: true
+                  })
+              })
+          } else {
+              this.setState({
+                  notes: [],
+                  loggedin: false
+              })
+          }
+          console.log(this.state.notes)
+      })
+
   }
 
-  //* *************** SHOW SIDE BAR ****************//
-  // CREATED A REFERENCE TO AN ELEMENT, USING A FUNCTION
-  // ADDED A TOGGLE CLASS
-  showSidebar(e) {
-    e.preventDefault();
-    this.sidebar.classList.toggle("show");
+  showSideBar(e) {
+      e.preventDefault();
+      this.sideBar.classList.toggle("show");
   }
 
-  //* *************** ADD NEW ITEM ****************//
   addNote(e) {
-    e.preventDefault();
-    const note = {
-      title: this.noteTitle.value,
-      text: this.noteText.value
-    };
+      e.preventDefault();
+      console.log('bob');
+      const note = {
+          title: this.noteTitle.value,
+          text: this.noteText.value
+      };
+      const userId = firebase.auth().currentUser.uid;
+      const dbRef = firebase.database().ref(`users/${userId}/notes`);
 
-    // MAKING A REFERENCE TO THE FIREBASE DATABASE
-    const dbRef = firebase.database().ref();
-    // PUSHING NOTE TO THE DB
-    dbRef.push(note);
+      dbRef.push(note);
 
-    // REFRESH INPUT FIELDS
-    this.noteTitle.value = "";
-    this.noteText.value = "";
-    this.showSidebar(e);
+      this.noteTitle.value = "";
+      this.noteText.value = "";
+      this.showSideBar(e);
   }
 
-  //* ***************** UPDATE ITEM ******************//
-  updateNote(e) {}
-  //* ***************** DELETE ITEM ******************//
   removeNote(noteId) {
-    const dbRef = firebase.database().ref(noteId);
-    dbRef.remove();
+      console.log(noteId);
+      const userId = firebase.auth().currentUser.uid;
+      const dbRef = firebase.database().ref(`users/${userId}/notes/${noteId}`);
+      dbRef.remove();
   }
 
-  // AUTHENTICATION
-  showCreate(e) {
-    e.preventDefault();
-    this.overlay.classList.toggle("show");
-    this.createUserModal.classList.toggle("show");
-  }
-  // CREATE AN ACCOUNT
   createUser(e) {
-    e.preventDefault();
-    // CHECK WHETHER THE PASSWORDS MATCH
-    // IF SO CREATE USER
-    const password = this.createPassword.value;
-    const confirm = this.confirmPassword.value;
-    const email = this.createEmail.value;
+      e.preventDefault();
+      const email = this.createEmail.value;
+      const password = this.createPassword.value;
+      const confirm = this.confirmPassword.value;
 
-    if (password === confirm) {
-      firebase
-        .auth()
-        .createUserWithEmailAndPassword(email, password)
-        .then(res => this.showCreate(e))
-        .catch(function(error) {
-          alert(error);
-        });
-    } else {
-      alert("Password must match");
-    }
+      console.log(email, password);
+
+      if (password === confirm) {
+          firebase.auth().createUserWithEmailAndPassword(email, password).then(res => this.showCreate(e)).catch(err => alert(err.message));
+      } else {
+          alert('password need to match')
+      }
+
   }
 
-  // SIGN IN USER
+  showCreate(e) {
+      e.preventDefault();
+      this.overlay.classList.toggle('show');
+      this.createUserModal.classList.toggle('show')
+  }
 
   showLogin(e) {
-    e.preventDefault();
-    this.overlay.classList.toggle("show");
-    this.loginModal.classList.toggle("show");
+      e.preventDefault();
+      this.overlay.classList.toggle('show');
+      this.loginModal.classList.toggle('show');
   }
 
   loginUser(e) {
-    e.preventDefault();
-    const email = this.userEmail.value;
-    const password = this.userPassword.value;
+      e.preventDefault();
+      const email = this.userEmail.value;
+      const password = this.userPassword.value;
 
-    firebase
-      .auth()
-      .signInWithEmailAndPassword(email, password)
-      .then(res => this.showLogin(e))
-      .catch(function(error) {
-        alert(error.message);
-      });
+      console.log(email + '  ' + password)
+
+      firebase.auth().signInWithEmailAndPassword(email, password).then(res => this.showLogin(e)).catch(err => alert(err.message))
   }
+
+  logOut() {
+      firebase.auth().signOut();
+  }
+
+  renderCards() {
+      if (this.state.loggedin) {
+          return this.state.notes.map((note, i) => {
+              return (
+                  <NoteCard note={note} key={`note-${i}`} removeNote={this.removeNote} />
+              )
+          }).reverse()
+      } else {
+          return (<h2>Login to add notes</h2>);
+      }
+  }
+
 
   // SIGNOUT USER
   //* **************** COMPONENT RENDER ****************//
   render() {
     return (
       <div>
-        <header className="mainHeader">
-          <h1>Xcute`</h1>
-          <nav>
-            <a href="" onClick={this.showSidebar}>
-              Add New Note
-            </a>
+          <header className="mainHeader">
+              <h1>Noted</h1>
+              <nav>
+                  {
+                      (() => {
+                          if (this.state.loggedin) {
+                              return (
+                                  <span>
+                                      <a href="" onClick={this.showSideBar}>Add New Note</a>
+                                      <a href="" onClick={this.logOut}>Logout</a>
+                                  </span>
+                              )
+                          } else {
+                              return (
+                                  <span>
+                                      <a href="" onClick={this.showCreate}>Create Account</a>
+                                      <a href="" onClick={this.showLogin}>Login</a>
+                                  </span>
+                              )
+                          }
+                      })()
+                  }
+              </nav>
+          </header>
+          <div className="overlay" ref={ref => this.overlay = ref}></div>
+          <section className="notes">
+              {this.renderCards()}
+          </section>
+          <aside className="sidebar" ref={ref => this.sideBar = ref}>
+              <form onSubmit={this.addNote}>
+                  <h3>Add New Note</h3>
+                  <div className="close-btn" onClick={this.showSideBar}>
+                      <i className="fa fa-times"></i>
+                  </div>
+                  <label htmlFor="note-title">Title</label>
+                  <input type="text" name="note-title" ref={ref => this.noteTitle = ref} />
+                  <label htmlFor="note-text">Text:</label>
+                  <textarea name="note-text" ref={ref => this.noteText = ref}></textarea>
+                  <input type="submit" value="Add New Note" />
+              </form>
+          </aside>
 
-            <a href="" onClick={this.showCreate}>
-              Create Account
-            </a>
-            <a href="" onClick={this.showLogin}>
-              Login
-            </a>
-          </nav>
-        </header>
-        <div className="overlay" ref={ref => (this.overlay = ref)} />
-        {/* NOTE CARD  */}
-        <section className="notes">
-          {this.state.notes
-            .map((note, i) => (
-              <NoteCard
-                note={note}
-                key={`note-${i}`}
-                removeNote={this.removeNote}
-                updateNote={this.updateNote}
-              />
-            ))
-            .reverse()}
-        </section>
-        <aside className="sidebar" ref={ref => (this.sidebar = ref)}>
-          <form onSubmit={this.addNote}>
-            <h3>Add New note</h3>
-            <div className="close-btn" onClick={this.showSidebar}>
-              <i className="fa fa-times" />
-            </div>
-            <label htmlFor="note-title">Title:</label>
-            <input
-              type="text"
-              name="note-title"
-              ref={ref => (this.noteTitle = ref)}
-            />
-            <label htmlFor="note-text">Note:</label>
-            <textarea
-              type="text"
-              name="note-text"
-              ref={ref => (this.noteText = ref)}
-            />
-            <input type="submit" value="Add New Note" />
-          </form>
-        </aside>
-        {/* LOGIN FORM */}
-        <div className="loginModal modal" ref={ref => (this.loginModal = ref)}>
-          <div className="close">
-            <i className="fa fa-times" onClick={this.showLogin} />
-          </div>
-          <form action="" onSubmit={this.loginUser}>
-            <div>
-              <label htmlFor="email">Email:</label>
-              <input
-                type="text"
-                name="email"
-                ref={ref => (this.userEmail = ref)}
-              />
-            </div>
-            <div>
-              <label htmlFor="password">Password:</label>
-              <input
-                type="password"
-                name="password"
-                ref={ref => (this.userPassword = ref)}
-              />
-            </div>
-            <div>
-              <input type="submit" value="Login" />
-            </div>
-          </form>
-        </div>
-        {/* CREATE USER */}
-        <div
-          className="createUserModal modal"
-          ref={ref => (this.createUserModal = ref)}
-        >
-          <div className="close" onClick={this.showCreate}>
-            <i className="fa fa-times" />
+          <div className="loginModal modal" ref={ref => this.loginModal = ref}>
+              <div className="close" onClick={this.showLogin}>
+                  <i className="fa fa-times"></i>
+              </div>
+              <form action="" onSubmit={this.loginUser}>
+                  <div>
+                      <label htmlFor="email">Email:</label>
+                      <input type="text" name="email" ref={ref => this.userEmail = ref} />
+                  </div>
+                  <div>
+                      <label htmlFor="password">Password:</label>
+                      <input type="password" name="password" ref={ref => this.userPassword = ref} />
+                  </div>
+                  <div>
+                      <input type="submit" value="Submit" />
+                  </div>
+              </form>
           </div>
 
-          <form action="" onSubmit={this.createUser}>
-            <div>
-              <label htmlFor="createEmail">Email:</label>
-              <input
-                type="text"
-                name="createEmail"
-                ref={ref => (this.createEmail = ref)}
-              />
-            </div>
-            <div>
-              <label htmlFor="createPassword">Password:</label>
-              <input
-                type="password"
-                name="createPassword"
-                ref={ref => (this.createPassword = ref)}
-              />
-            </div>
-            <div>
-              <label htmlFor="confirmPassword">Confirm Password:</label>
-              <input
-                type="password"
-                name="confirmPassword"
-                ref={ref => (this.confirmPassword = ref)}
-              />
-            </div>
-            <div>
-              <input type="submit" value="Create" />
-            </div>
-          </form>
-        </div>
+          <div className="createUserModal modal" ref={ref => this.createUserModal = ref}>
+              <div className="close" onClick={this.showCreate}>
+                  <i className="fa fa-times"></i>
+              </div>
+              <form action="" onSubmit={this.createUser}>
+                  <div>
+                      <label htmlFor="createEmail">Email:</label>
+                      <input type="text" name="createEmail" ref={ref => this.createEmail = ref} />
+                  </div>
+                  <div>
+                      <label htmlFor="createPassword">Password:</label>
+                      <input type="password" name="createPassword" ref={ref => this.createPassword = ref} />
+                  </div>
+                  <div>
+                      <label htmlFor="confirmPassword">Confirm Password:</label>
+                      <input type="password" name="confirmPassword" ref={ref => this.confirmPassword = ref} />
+                  </div>
+                  <div>
+                      <input type="submit" value="Create" />
+                  </div>
+              </form>
+          </div>
       </div>
-    );
-  }
+  )
+}
 }
 
 export default App;
